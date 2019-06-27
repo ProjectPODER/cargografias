d959dfcde124bf7ec40cfa7a07961ac7817ffd6e
Marisol Carrillo <marisol@rindecuentas.org>
2019-06-27 17:19:33 -0500

    Adding new content

--- web/js/flujosilicitos.js
+++ web/js/flujosilicitos.js
@@ -4,3 +4,25 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
+
+  // Add scrollspy to <body>
+$('body').scrollspy({target: ".sidenav", offset: 50});
+
+// Add smooth scrolling on all links inside the navbar
+$(".sidenav a").on('click', function(event) {
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 20
+    }, 500, function(){
+    });
+  }  // End if
+});
